e4ec0f8b8022aabf12a5a9cab025a3ad5fc6b113
Mark Pospesel <mpospese@gmail.com>
2012-10-04 06:25:58 -0400

    Initial Commit

---
+++ IntroducingCollectionViews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IntroducingCollectionViews
+//
+//  Created by Mark Pospesel on 10/4/12.
+//  Copyright (c) 2012 Mark Pospesel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IntroducingCollectionViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IntroducingCollectionViews
+//
+//  Created by Mark Pospesel on 10/4/12.
+//  Copyright (c) 2012 Mark Pospesel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
